9dec8c5012d1420b54aa78419347f59265bd0dd8
Zayed Uddin <upal.core2@gmail.com>
2014-08-13 17:19:03 +0600

    review list is partialy complete

--- OnlineKenakata/Review.h
+++ OnlineKenakata/Review.h
@@ -10,9 +10,16 @@
 #import "EDStarRating.h"
 
 @interface Review : UIViewController<UITableViewDataSource,UITableViewDelegate>{
-    
+    BOOL isReadMoreButtonTouched[5];
+
 }
 @property (strong,nonatomic)IBOutlet EDStarRating *TotalRating;
 @property (strong,nonatomic) IBOutlet UILabel *reviewCount;
 @property (strong,nonatomic) IBOutlet UILabel *numberOfStar;
+@property (strong,nonatomic) IBOutlet UIScrollView *scrollview;
+@property (strong,nonatomic) IBOutlet UIView *containView;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
+@property (strong,nonatomic) IBOutlet UITableView *tableview;
+@property (strong,nonatomic) IBOutlet UIView *barContainer;
+
 @end
--- OnlineKenakata/Review.m
+++ OnlineKenakata/Review.m
@@ -7,13 +7,18 @@
 //
 
 #import "Review.h"
+#import "TextStyling.h"
+
+#define rowHeight 130;
 
 @interface Review ()
+
+
 
 @end
 
 @implementation Review
-
+int indexOfReadMoreButton;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -23,12 +28,32 @@
     return self;
 }
 
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    float height=30+192+5*rowHeight;
+    if(isReadMoreButtonTouched [0]){
+        NSLog(@"true :(");
+    }
+    [self updateHeight:height];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self setValueOntop];
-    // Do any additional setup after loading the view.
+    [self statBar];
+    
+   
 }
+
+-(void)updateHeight:(int)height{
+    self.heightConstraint.constant =height; //self.description.contentSize.height;
+    
+    
+    [self.scrollview setContentSize:CGSizeMake(320, height)];
+}
+    // Do any additional setup after loading the view.
+
 -(void)setValueOntop{
     [self StarSetter:self.TotalRating number:3.7];
 }
@@ -49,13 +74,143 @@
     
     UILabel *titile=(UILabel *)[cell viewWithTag:901];
     titile.text=@"This product is very good , i am in love with this product";
+    UITextView *description=(UITextView *)[cell viewWithTag:904];
+
+    UIButton *btn=(UIButton *)[cell viewWithTag:905];
+   
+
+    if(indexPath.row==3){
+        description.text=@"good";
+    }
+    
+    NSString *string =description.text;//[self.productData objectForKey:@"description"];
+    NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:12]};
+    NSString *str=@"kfhkjfhkf kfhkf sfkh kfshksfg skfh skfskfhskfhskfhksfhkfshksfhksfhskfhskf ksfhsfkh skfhsdkfh skf hk skfhksfhg sfkhgsdfiugs sfhsiusiruh sifh shishfvsuhviusfhgurruh ifhfurhrufh aeuhfurh rufhufurfhur irfhrif ";
+    CGRect rect = [string boundingRectWithSize:CGSizeMake(description.frame.size.width, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
+    
+    if(isReadMoreButtonTouched[indexOfReadMoreButton] && [indexPath row]== indexOfReadMoreButton)
+    {
+        NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:12]};
+        
+        CGRect rect = [str boundingRectWithSize:CGSizeMake(description.frame.size.width, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
+        
+   
+    
+        [description setFrame:rect];
+        description.attributedText=[TextStyling AttributForDescription:str];
+
+    }else{
+        
+        if(rect.size.height<=description.frame.size.height){
+            
+            btn.hidden=YES;
+            
+        }
+        
+        
+    }
+   
+  
+   
+    
     
     EDStarRating *star=(EDStarRating *)[cell viewWithTag:902];
     [self StarSetter:star number:(float)indexPath.row];
     
+    btn.tag=indexPath.row;
+    
+    isReadMoreButtonTouched[indexPath.row]=NO;
     return cell;
 }
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(isReadMoreButtonTouched[indexOfReadMoreButton] && [indexPath row]== indexOfReadMoreButton){
+        [self updateHeight: self.heightConstraint.constant+130];
+        return 300.0f;
+    }
+    else return rowHeight;
+}
 
+-(IBAction)remoreButtonClicked:(id)sender{
+    UIButton *readMoreButton = (UIButton *)sender;
+    indexOfReadMoreButton=[readMoreButton tag];
+    isReadMoreButtonTouched[indexOfReadMoreButton]=!isReadMoreButtonTouched[indexOfReadMoreButton];
+    
+    [[self tableview] beginUpdates];
+    [[self tableview] reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForItem: indexOfReadMoreButton inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+    [[self tableview] endUpdates];
+}
+
+-(void)statBar{
+    int x =0;
+    int y=5;
+    int diffY=20;
+    float totalWidth=self.barContainer.frame.size.width;
+    int totalVote=1854;
+    float x5=720.0;
+    float x4=410.0;
+    float x3=12;
+    float x2=346.0;
+    float x1=366;
+    
+    float ratio=totalWidth/totalVote;
+    
+    NSLog(@"%f   ,%f",ratio,totalWidth);
+
+    UIView *bar11=[[UIView alloc]initWithFrame:CGRectMake(x, y, ratio*x5, 10)];
+    [bar11 setBackgroundColor:[UIColor orangeColor]];
+    
+    UIView *bar21=[[UIView alloc]initWithFrame:CGRectMake((x+ratio*x5), y, totalWidth-ratio*x5, 10)];
+    [bar21 setBackgroundColor:[UIColor grayColor]];
+    
+    
+    y+=diffY;
+
+    UIView *bar12=[[UIView alloc]initWithFrame:CGRectMake(x, y, ratio*x4, 10)];
+    [bar12 setBackgroundColor:[UIColor orangeColor]];
+
+    
+    UIView *bar22=[[UIView alloc]initWithFrame:CGRectMake((x+ratio*x4), y, totalWidth-ratio*x4, 10)];
+    [bar22 setBackgroundColor:[UIColor grayColor]];
+
+    y+=diffY;
+    UIView *bar13=[[UIView alloc]initWithFrame:CGRectMake(x, y, ratio*x3, 10)];
+    [bar13 setBackgroundColor:[UIColor orangeColor]];
+    
+    UIView *bar23=[[UIView alloc]initWithFrame:CGRectMake((x+ratio*x3), y, totalWidth-ratio*x3, 10)];
+    [bar23 setBackgroundColor:[UIColor grayColor]];
+
+    y+=diffY;
+    UIView *bar14=[[UIView alloc]initWithFrame:CGRectMake(x, y, ratio*x2, 10)];
+    [bar14 setBackgroundColor:[UIColor orangeColor]];
+    
+    UIView *bar24=[[UIView alloc]initWithFrame:CGRectMake((x+ratio*x2), y, totalWidth-ratio*x2, 10)];
+    [bar24 setBackgroundColor:[UIColor grayColor]];
+
+    y+=diffY;
+    UIView *bar15=[[UIView alloc]initWithFrame:CGRectMake(x, y, ratio*x1, 10)];
+    [bar15 setBackgroundColor:[UIColor orangeColor]];
+
+    
+    UIView *bar25=[[UIView alloc]initWithFrame:CGRectMake((x+ratio*x1), y, totalWidth-ratio*x1, 10)];
+    [bar25 setBackgroundColor:[UIColor grayColor]];
+    
+    
+    [self.barContainer addSubview:bar11];
+    [self.barContainer addSubview:bar12];
+    [self.barContainer addSubview:bar13];
+    [self.barContainer addSubview:bar14];
+    [self.barContainer addSubview:bar15];
+
+
+    [self.barContainer addSubview:bar21];
+    [self.barContainer addSubview:bar22];
+    [self.barContainer addSubview:bar23];
+    [self.barContainer addSubview:bar24];
+    [self.barContainer addSubview:bar25];
+
+    
+
+}
 
 -(void)StarSetter:(EDStarRating *)star number:(float) number{
     star.starImage=[UIImage imageNamed:@"star.png"];
@@ -72,6 +227,7 @@
     [star setNeedsDisplay];
 
 }
+
 /*
 #pragma mark - Navigation
 
