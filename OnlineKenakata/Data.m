1e54bc781cdadfa49fb228c45bdfc5f46fecab79
rabby29 <rabbyalam@gmail.com>
2014-09-09 17:54:30 +0600

    app code changed

--- OnlineKenakata/Data.m
+++ OnlineKenakata/Data.m
@@ -9,7 +9,7 @@
 #import "Data.h"
 
 NSString * const baseurl=@"http://online-kenakata.com/mobile_api";
-NSString * const appcode=@"1023";
+NSString * const appcode=@"1000";
 @implementation Data
 
 +(NSString *)getAppCode{
